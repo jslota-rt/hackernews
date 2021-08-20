0ceb2710abc4db5679946deec69d8d9ef23816eb
John Slota <jslota@reservetrust.com>
2021-08-20 09:50:32 -0700

    got db connected and working

--- server.go
+++ server.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"github.com/jslota-rt/hackernews/graph"
+	hackernews "github.com/jslota-rt/hackernews/graph/generated"
+	database "github.com/jslota-rt/hackernews/internal/pkg/db/mysql"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/jslota-rt/hackernews/graph"
-	"github.com/jslota-rt/hackernews/graph/generated"
+	"github.com/go-chi/chi"
 )
 
 const defaultPort = "8080"
@@ -19,11 +21,14 @@ func main() {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	router := chi.NewRouter()
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	database.InitDB()
+	database.Migrate()
+	server := handler.NewDefaultServer(hackernews.NewExecutableSchema(hackernews.Config{Resolvers: &graph.Resolver{}}))
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", server)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
